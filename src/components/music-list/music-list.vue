4057216dac4ffc048e7e886aec28cb8caee2ba09
Lin <ljw636@126.com>
2019-03-24 23:53:25 +0800

    歌手详情页(music-list组件)开发完成

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper">
+        <div v-show="songs.length>0" class="play" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" class="list" :data="songs"
             ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -20,8 +29,12 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import Loading from 'base/loading/loading'
+  import { prefixStyle } from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
 
   export default {
     // 接受数据
@@ -63,29 +76,51 @@
     methods: {
       scroll (pos) {
         this.scrollY = pos.y
+      },
+      back () {
+        this.$router.back()
       }
     },
     watch: {
       scrollY (newY) {
         let translateY = Math.max(this.minTranslateY, newY)
         let zIndex = 0
-        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-        this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+        let scale = 1
+        let blur = 0
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+
+        const percent = Math.abs(newY / this.imageHeight)
+
+        // 当往下滑时 图片比例跟着放大
+        // 当往上滑时 图片有高斯模糊的效果
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+        // 设置高斯模糊属性
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+
         // 如果歌曲列表滚动到顶部 设置图片层为顶部
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           // 恢复图片状态
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     components: {
       Scroll,
+      Loading,
       SongList
     }
   }
